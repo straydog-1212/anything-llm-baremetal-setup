6773d82b9b9812c44163905a9d8ce7987b762920
lulu <56979669+straydog-1212@users.noreply.github.com>
2024-05-05 03:09:18 -0700

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export INSTALL_DIR="/opt/anything-llm"
 export STORAGE_DIR="$INSTALL_DIR/storage"
-cd "$INSTALL_DIR"/server && NODE_ENV=production node index.js &
-cd "$INSTALL_DIR"/collector && NODE_ENV=production node index.js &
+cd "$INSTALL_DIR"/server/utils/files && NODE_ENV=production node index.js &
+cd "$INSTALL_DIR"/collector/utils/files/ && NODE_ENV=production node index.js &
