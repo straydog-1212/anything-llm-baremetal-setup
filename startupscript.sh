eeebb8d0d713eaf913c7ec1267bbe7cc591f18c5
lulu <56979669+straydog-1212@users.noreply.github.com>
2024-05-05 02:25:16 -0700

    Create startupscript.sh
    made startup script

---
+++ startupscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+INSTALL_DIR="/opt/anything-llm
+export STORAGE_DIR="$INSTALL_DIR/storage
+cd $INSTALL_DIR/server && NODE_ENV=production node index.js &
+cd $INSTALL_DIR/collector && NODE_ENV=production node index.js &
